462bed9021e489185292308abffaa9d2d069e938
Jordi Gerona <jordi9@trovit.com>
2014-10-07 16:32:19 +0200

    Set JAVA_HOME and lang variables

--- plugins/me/me.plugin.zsh
+++ plugins/me/me.plugin.zsh
@@ -6,6 +6,13 @@ export HOMEBREW_CASK_OPTS="--caskroom=/opt/apps"
 # The PATH
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:~/.bin
 
+# Set desired java version
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_60.jdk/Contents/Home
+
+# Set locale properly
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
 # Aliases
 #########
 alias homecnf="cd ~/.homesick/repos/dotfiles"
