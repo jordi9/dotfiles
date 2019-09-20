45fc3e905266b5fd799953e1e44e9e4977636353
Jordi Gerona <jordi.gerona@zopa.com>
2019-09-20 09:36:57 +0200

    Aliases for kubectl

--- home/.zsh/prompt.zsh
+++ home/.zsh/prompt.zsh
@@ -3,3 +3,6 @@ setopt correct
 export SPACESHIP_TIME_SHOW=true
 export SPACESHIP_BATTERY_SHOW=false
 export SPACESHIP_KUBECONTEXT_SHOW=false
+
+alias show-kube-context='SPACESHIP_KUBECONTEXT_SHOW=true'
+alias hide-kube-context='SPACESHIP_KUBECONTEXT_SHOW=false'
