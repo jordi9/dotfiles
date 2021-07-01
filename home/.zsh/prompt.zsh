8c124002fd994292992e2ed74042f28ea47acda0
Jordi Gerona <jordi.gerona@team.bumble.com>
2021-07-01 21:09:52 +0200

    Disable JVM and docker icons

--- home/.zsh/prompt.zsh
+++ home/.zsh/prompt.zsh
@@ -3,6 +3,8 @@ setopt correct
 export SPACESHIP_TIME_SHOW=true
 export SPACESHIP_BATTERY_SHOW=false
 export SPACESHIP_KUBECONTEXT_SHOW=false
+export SPACESHIP_DOCKER_SHOW=false
+export SPACESHIP_GRADLE_JVM_SHOW=false
 
 alias show-kube-context='SPACESHIP_KUBECONTEXT_SHOW=true'
 alias hide-kube-context='SPACESHIP_KUBECONTEXT_SHOW=false'
