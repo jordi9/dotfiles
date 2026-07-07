92d37abda1d4a85d95988d8b8f89a169a8ea11ce
Jordi Gerona <jordi@donky.org>
2026-07-07 15:29:29 +0200

    One pnmorep time

--- home/.zsh/tools.zsh
+++ home/.zsh/tools.zsh
@@ -57,8 +57,7 @@ esac
 
 # pnpm
 export PNPM_HOME="$HOME/Library/pnpm"
-case ":$PATH:" in
-  *":$PNPM_HOME:"*) ;;
-  *) export PATH="$PNPM_HOME:$PATH" ;;
-esac
+path=("$PNPM_HOME/bin" "$PNPM_HOME" $path)
+typeset -U path
+export PATH
 # pnpm end
